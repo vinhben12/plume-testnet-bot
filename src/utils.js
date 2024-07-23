107b41b8d097861ad951fbc3f77ffb46719c895c
beacon <75365338+vinhben12@users.noreply.github.com>
2024-07-23 11:39:15 +0700

    utils.js

--- src/utils.js
+++ src/utils.js
@@ -7,8 +7,6 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=          Plume Testnet Bot           ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
